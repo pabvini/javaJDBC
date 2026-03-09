55414bc79bb793157a3aaa094e980d2f8f471c1b
pabvini <pv3394719@gmail.com>
2026-03-09 15:32:55 -0300

    Adição de código no Main para testar todos as funções.

--- src/main/java/com/example/teste/Main.java
+++ src/main/java/com/example/teste/Main.java
@@ -6,7 +6,14 @@ import java.sql.Connection;
 public class Main {
 	public static void main(String[] args) {
 
+		CreateStudent.insertStudent("Sonic", 30, "sonic@gmail.com");
+		CreateStudent.insertStudent("Mario", 30, "mario@hotmail.com");
 
+		ReadStudent.getAllStudent();
+
+		UpdateStudent.updateStudent(1, "sonic@hotmail.com");
+
+		DeleteStudent.deleteStudent(2);
 
 	}
 }
